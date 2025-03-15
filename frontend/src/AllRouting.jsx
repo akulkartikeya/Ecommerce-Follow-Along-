bfbdbe91f4d654cd966455b3bf6bdbb02f4b032b
DevWebAbhi <tiwariabhishek889912@gmail.com>
2025-03-15 12:13:54 +0530

    Done

---
+++ frontend/src/AllRouting.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Route,Routes} from "react-router-dom";
+import Products from './components/Products';
+import Login from './components/Login';
+import Signup from './components/Signup';
+const AllRouting = () => {
+  return (
+    <Routes>
+        <Route path='/' element={<Products/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+    </Routes>
+  )
+}
+
+export default AllRouting
