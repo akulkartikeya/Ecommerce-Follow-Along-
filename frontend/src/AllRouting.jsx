b7210b1630f40fa62e44b86908bd93575ece00f8
akulkarthikeya006@gmail.com <akulkarthikeya006@gmail.com>
2025-04-19 12:03:22 +0530

    mil22

---
+++ frontend/src/AllRouting.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Route,Routes} from "react-router-dom";
+import Products from './components/Products';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import AddProduct from './components/AddProduct';
+import MyProducts from './components/MyProducts';
+import Cart from "./components/Cart"
+const AllRouting = () => {
+  return (
+    <Routes>
+        <Route path='/' element={<Products/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/addproducts' element={<AddProduct/>}/>
+        <Route path='/myproducts' element={<MyProducts/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+    </Routes>
+  )
+}
+
+export default AllRouting
